7b04f587c2406cc3266affa65ed88051aca10c4f
Rinoa Heartilly <kissy0227@yahoo.com.au>
2018-08-10 21:28:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    var total = total()
+    var total = total();
     //return "Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + "."
   }
 }
