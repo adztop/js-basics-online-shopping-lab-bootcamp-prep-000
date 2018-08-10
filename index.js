2173fc626ff71e0313b4e634d9104bd94cb39145
Rinoa Heartilly <kissy0227@yahoo.com.au>
2018-08-10 21:21:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,6 +83,6 @@ function placeOrder(cardNumber) {
   }
   else {
     //cart = []
-    return "Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + "."
+    console.log("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".")
   }
 }
