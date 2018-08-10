66c352804e5fef17aa0313cb9668c52ff8842489
Rinoa Heartilly <kissy0227@yahoo.com.au>
2018-08-10 20:30:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ function removeFromCart(item) {
   for(let i = 0; i < cart.length; i++){
     if(cart[i]["itemName"] === item){
       cart = cart.splice(i, 1)
-      return cart;
     }
+  return cart
   }
 }
 
