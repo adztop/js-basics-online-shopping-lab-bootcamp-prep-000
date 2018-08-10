5875635c79864c6e9e76ec5a06423c403b3f2c48
Rinoa Heartilly <kissy0227@yahoo.com.au>
2018-08-10 21:16:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ function removeFromCart(item){
       cart.splice(i, 1);
       return cart
     }
-    
   }
+  return "That item is not in your cart."
 }  
 function placeOrder(cardNumber) {
   // write your code here
